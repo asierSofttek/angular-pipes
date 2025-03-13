a2c20a378f72439cd6d4ea0450da1f3be625848f
Asier Zabala <asier.zabala@softtek.com>
2025-03-13 09:21:56 +0100

    add filter pipes

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -6,10 +6,13 @@ import { heroes } from '../../data/heroes.data';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, HeroCreatorPipe, TitleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, HeroCreatorPipe, TitleCasePipe, HeroSortByPipe, HeroFilterPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
@@ -24,6 +27,10 @@ export default class CustomPageComponent {
   }
 
   heroes = signal(heroes);
+
+  sortBy = signal<keyof Hero | null>(null);
+
+  searchQuery = signal('')
 
 
 }
